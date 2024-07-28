f2a7131ed2949ee32d56151aacdc72389034e627
SobkoSergiy <143492394+SobkoSergiy@users.noreply.github.com>
2024-07-28 17:43:43 +0300

    Update Car_video_detection.py

--- app/src/ocr/video1/Car_video_detection.py
+++ app/src/ocr/video1/Car_video_detection.py
@@ -5,33 +5,6 @@ import numpy as np
 car_cascade = cv2.CascadeClassifier("haarcascade_russian_plate_number.xml")
 # https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
 
-'''
-cap = cv2.VideoCapture('license_plate.mp4')
-# cap = cv2.VideoCapture(0)
-cap.read('../data/d10.jpg')
-cap.set(3, 640)  # set Width
-cap.set(4, 480)  # set Height
-
-while True:
-    ret, img = cap.read()
-    img = cv2.flip(img, -1)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cars = car_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5,  minSize=(20, 20))
-
-    for (x, y, w, h) in cars:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = img[y:y + h, x:x + w]
-
-    cv2.imshow('cars', img)
-
-    if (cv2.waitKey(30) & 0xff) == 27:  # press 'ESC' to quit
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-'''
-
 cap = cv2.VideoCapture('license_plate.mp4')
 # cap = cv2.VideoCapture('testvideo.mp4')
 # cap = cv2.VideoCapture('cars.mp4')
